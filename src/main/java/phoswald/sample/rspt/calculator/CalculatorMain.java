85d8bcc30c3a16881a1843cd8415c1488a08ceeb
Philip Oswald <philip.oswald@gmail.com>
2016-04-23 14:11:21 +0200

    Improve main and add tests

--- src/main/java/phoswald/sample/rspt/calculator/CalculatorMain.java
+++ src/main/java/phoswald/sample/rspt/calculator/CalculatorMain.java
@@ -1,30 +1,38 @@
 package phoswald.sample.rspt.calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import phoswald.sample.rspt.calculator.parser.CalculatorParser;
 import phoswald.sample.rspt.calculator.parser.CalculatorParser.Ref;
 import phoswald.sample.rspt.calculator.parser.CalculatorParser.Ref_int;
 
 public class CalculatorMain {
 
-	public static void main(String[] args) {
-		System.out.println("CalculatorConsole -- An expression evaluator to showcase RSPT (the Really Simple Parser Tool)");
-		System.out.println("(Enter expressions to calculate or 'exit' to quit)");
+	public static void main(String[] args) throws IOException {
+		System.out.println("sample-rspt-calculator -- A simple calculator using RSPT (the Really Simple Parser Tool)");
+		System.out.println("[Enter expressions to calculate or an empty line to quit]");
+		BufferedReader systemIn = new BufferedReader(new InputStreamReader(System.in));
 		CalculatorParser parser = new CalculatorParser();
 		while(true) {
-			String      input  = System.console().readLine();
+		    String input = systemIn.readLine();
+		    if(input == null || input.isEmpty()) {
+		        break;
+		    }
 			Ref<String> result = new Ref<String>(null);
-			Ref_int     error  = new Ref_int(0);
-			if(input.equals("exit")) {
-				break;
-			}
+			Ref_int error  = new Ref_int(0);
 			if(!parser.Parse_ROOT(input, result, error)) {
-	            String next = input.length()-error.val <= 50 ? input.substring(error.val, input.length()) : input.substring(error.val, error.val + 50) + "...";
-				String msg = String.format("Error at offset %d: Cannot handle '%s'.", error.val+1, next);
-				System.out.println(msg);
+                System.out.println("Syntax Error:");
+			    System.out.println("- input:   '" + input + "'");
+			    System.out.println("- position: " + pad(' ', error.val) + "^");
 			} else {
-				String msg = "Result: " + result.val;
-				System.out.println(msg);
+				System.out.println("Result: " + result.val);
 			}
 		}
+	}
+
+	private static String pad(char character, int length) {
+	    return new String(new char[length]).replace('\u0000', character);
 	}
 }
---
+++ src/test/java/phoswald/sample/rspt/calculator/CalculatorParserTest.java
@@ -0,0 +1,60 @@
+package phoswald.sample.rspt.calculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import phoswald.sample.rspt.calculator.parser.CalculatorParser;
+import phoswald.sample.rspt.calculator.parser.CalculatorParser.Ref;
+import phoswald.sample.rspt.calculator.parser.CalculatorParser.Ref_int;
+
+public class CalculatorParserTest {
+
+    private CalculatorParser testee = new CalculatorParser();
+
+    @Test
+    public void testPredefinedSymbols() {
+        assertResult(Math.PI, "pi");
+        assertResult(0.0, "x");
+        assertResult("Copyright (C) 2013 Philip Oswald", "About");
+        assertResult("Version 1.11 for Java", "Version");
+    }
+
+    @Test
+    public void testSimpleMath() {
+        assertResult("3.0", "1+2");
+    }
+
+    @Test
+    public void testStatefulVariables() {
+        assertResult("3.0", "x=1+2");
+        assertResult("6.0", "y=x+x");
+        assertResult("36.0", "y=y*y");
+        assertResult("36.0", "y");
+    }
+
+    @Test
+    public void testSyntaxErrors() {
+        assertError(1, "1+");
+    }
+
+    private void assertResult(double expectedResult, String expression) {
+        assertResult(Double.toString(expectedResult), expression);
+    }
+
+    private void assertResult(String expectedResult, String expression) {
+        Ref<String> actualResult = new Ref<>(null);
+        Ref_int pos = new Ref_int(0);
+        assertTrue(testee.Parse_ROOT(expression, actualResult, pos));
+        assertEquals(expectedResult, actualResult.val);
+    }
+
+    private void assertError(int expectedOffset, String expression) {
+        Ref<String> actualResult = new Ref<>(null);
+        Ref_int pos = new Ref_int(0);
+        assertFalse(testee.Parse_ROOT(expression, actualResult, pos));
+        assertEquals(expectedOffset, pos.val);
+    }
+}
